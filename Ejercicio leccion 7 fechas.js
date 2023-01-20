b2b15d8ac6ae9c845f4f84f31ea3da3855a56896
Santi <121914283+rpssanti@users.noreply.github.com>
2023-01-20 11:15:21 +0100

    Add files via upload

---
+++ Ejercicio leccion 7 fechas.js
@@ -0,0 +1,19 @@
+//Ejercicio leccion 7 fecha
+
+
+const fecha = new Date("January 20 , 2023");
+
+const fecha2 = new Date("October2 , 1990")
+
+console.log(fecha > fecha2)
+
+console.log(fecha2)
+
+console.log(fecha2.getDate())
+
+console.log(fecha2.getMonth()+1)
+
+console.log(fecha2.getFullYear())
+
+
+
